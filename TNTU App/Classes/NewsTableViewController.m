8f020e6dd6101e8ed442607b4f0503059bd97a20
dimacheverda <dima.cheverda@gmail.com>
2013-12-03 00:27:23 +0200

    add MBProgressHUD

--- TNTU App/Classes/NewsTableViewController.m
+++ TNTU App/Classes/NewsTableViewController.m
@@ -12,12 +12,15 @@
 #import "NewsViewController.h"
 #import "TntuKit.h"
 #import "UIImageView+AFNetworking.h"
+#import "Reachability.h"
+#import "MBProgressHUD.h"
 
-@interface NewsTableViewController ()
+@interface NewsTableViewController () <MBProgressHUDDelegate>
 
 @property (nonatomic) BOOL isDataLoaded;
 @property (nonatomic) NSInteger selectedCellIndex;
 @property (strong, nonatomic) UIRefreshControl *refreshControl;
+@property (strong, nonatomic) MBProgressHUD *HUD;
 
 @end
 
@@ -33,26 +36,74 @@
     [self.refreshControl addTarget:self action:@selector(refreshControlToggled) forControlEvents:UIControlEventValueChanged];
     [self.tableView addSubview:self.refreshControl];
     
+    // Init MBProgressHUD
+    self.HUD = [[MBProgressHUD alloc] initWithView:self.tableView];
+    [self.tableView addSubview:self.HUD];
+    
     // Loading data in background thread and reloading table view data source
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        self.tableViewDataArray = [TntuKit loadArrayFromDropboxFile:@"newsbase.plist"];
-        dispatch_async(dispatch_get_main_queue(), ^{
-            self.isDataLoaded = YES;
-            [self.tableView reloadData];
-        });
-    });
+    [self refreshControlToggled];
 }
 
 - (void)refreshControlToggled
 {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        self.tableViewDataArray = [TntuKit loadArrayFromDropboxFile:@"newsbase.plist"];
-        dispatch_async(dispatch_get_main_queue(), ^{
-            self.isDataLoaded = YES;
-            [self.tableView reloadData];
-            [self.refreshControl endRefreshing];
+    // Allocate a reachability object
+    Reachability* reach = [Reachability reachabilityWithHostname:@"www.google.com"];
+    
+    // Set the blocks
+    reach.reachableBlock = ^(Reachability*reach)
+    {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+            
+            // Showing HUD
+            dispatch_async(dispatch_get_main_queue(), ^{
+            
+                // Set custom view mode
+                self.HUD.mode = MBProgressHUDModeIndeterminate;
+                self.HUD.delegate = self;
+                self.HUD.labelText = @"Loading";
+                
+                [self.HUD show:YES];
+            });
+            
+            // Loading array fomr internet
+            self.tableViewDataArray = [TntuKit loadArrayFromDropboxFile:@"newsbase.plist"];
+            
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+            
+            // Hiding HUD
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.HUD hide:YES];
+            });
+            
+            if (self.tableViewDataArray != nil) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    self.isDataLoaded = YES;
+                    [self.tableView reloadData];
+                    [self.refreshControl endRefreshing];
+                });
+            }
         });
-    });
+    };
+    
+    reach.unreachableBlock = ^(Reachability*reach)
+    {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.refreshControl endRefreshing];
+            
+            // Set custom view mode
+            self.HUD.mode = MBProgressHUDModeCustomView;
+            self.HUD.delegate = self;
+            self.HUD.labelText = @"No Network Connection";
+            
+            [self.HUD show:YES];
+            [self.HUD hide:YES afterDelay:1.5];
+        });
+    };
+    
+    // Start the notifier, which will cause the reachability object to retain itself!
+    [reach startNotifier];
 }
 
 #pragma mark - Table view data source
